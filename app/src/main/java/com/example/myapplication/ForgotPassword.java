bd82f4d9536110496fe6712e4fdac6f0c436cb42
uday <udaydheerajreddy@gmail.com>
2022-05-26 16:36:05 -0400

    updated

--- app/src/main/java/com/example/myapplication/ForgotPassword.java
+++ app/src/main/java/com/example/myapplication/ForgotPassword.java
@@ -1,14 +1,26 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ForgotPassword extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassword extends AppCompatActivity implements View.OnClickListener {
     public Button button;
+    private EditText emailEditText;
+
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +30,53 @@ public class ForgotPassword extends AppCompatActivity {
 
 
         button = (Button) findViewById(R.id.verifybtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ForgotPassword.this,EmailidVerification.class);
-                startActivity(intent);
-    }
-});
+        button.setOnClickListener(this);
         button = (Button) findViewById(R.id.cancel_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(this);
+        emailEditText = (EditText) findViewById(R.id.emailid);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cancel_button:
+                startActivity(new Intent(ForgotPassword.this,MainActivity.class));
+                break;
+            case R.id.verifybtn:
+                resetPassword();
+                break;
+    }
+
+}
+
+    private void resetPassword() {
+
+        String email = emailEditText.getText().toString().trim();
+
+        if (email.isEmpty()){
+            emailEditText.setError("Email Is required!");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Please Enter Valid Email!");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ForgotPassword.this,MainActivity.class);
-                startActivity(intent);
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(ForgotPassword.this,"Reset Password Emil sent Successfully!", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(ForgotPassword.this,"Something Went Wrong", Toast.LENGTH_LONG).show();
+                }
             }
         });
-
-
-    }}
+    }
+    }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public Button button;
-    public TextView button1;
+    public TextView textView;
+    private EditText editTextEmail,editTextPassword;
+    public ImageView imageView;
 
+    private FirebaseAuth mAuth;
 
     @Override
 
@@ -20,33 +34,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.loginbtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SelectionPage.class);
-                startActivity(intent);
-
-            }
-
-        });
+        button.setOnClickListener(this);
         button = (Button) findViewById(R.id.signupbtnmain);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, signupScreen.class);
-                startActivity(intent);
+        button.setOnClickListener(this);
+        textView = (TextView) findViewById(R.id.forgotpass);
+        textView.setOnClickListener(this);
 
-            }
+        editTextEmail = (EditText) findViewById(R.id.username);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        imageView = (ImageView) findViewById(R.id.signInWithGoogle);
+        imageView.setOnClickListener(this);
 
-        });
-        button1 = (TextView) this.findViewById(R.id.forgotpass);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ForgotPassword.class);
-                startActivity(intent);
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
 
     }
-}
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginbtn:
+                userLogin();
+                break;
+            case R.id.signupbtnmain:
+                startActivity(new Intent(MainActivity.this, signupScreen.class));
+                break;
+            case R.id.forgotpass:
+                startActivity(new Intent(MainActivity.this, ForgotPassword.class));
+                break;
+
+
+        }
+
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()){
+            editTextEmail.setError("Email Is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please Enter Valid Email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()){
+            editTextPassword.setError("Password Is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length()<6) {
+            editTextPassword.setError("Password Is less than 6 char");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (user.getEmail().contains("admin")){
+                            startActivity(new Intent(MainActivity.this, AdminActivity.class));
+                        }else {
+                            startActivity(new Intent(MainActivity.this, SelectionPage.class));
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this,"Login Failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+        }
+
+    }
