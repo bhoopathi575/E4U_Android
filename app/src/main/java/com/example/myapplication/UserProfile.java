802bf4a4e6f24431b057be36630286e9641d7fb6
bhoopathi <bhoopathi575@gmail.com>
2022-05-26 16:30:45 -0400

    updated

--- app/src/main/java/com/example/myapplication/UserProfile.java
+++ app/src/main/java/com/example/myapplication/UserProfile.java
@@ -1,14 +1,30 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myapplication.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserProfile extends AppCompatActivity {
     public Button button;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private  String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +33,47 @@ public class UserProfile extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        button = (Button) findViewById(R.id.signupbtn);
+        button = (Button) findViewById(R.id.signoutbtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(UserProfile.this, MainActivity.class);
-                startActivity(intent);
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(UserProfile.this, SplashActivity.class));
 
 
             }
 
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userId = user.getUid();
+
+        final TextView userNameTextView = (TextView) findViewById(R.id.username);
+        final TextView useremailTextView = (TextView) findViewById(R.id.email);
+        final TextView userMobileNumberTextView = (TextView) findViewById(R.id.usermobilenumber);
+
+        reference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if (userProfile!= null){
+                    String username = userProfile.fullname;
+                    String email = userProfile.emailid;
+                    String mobileno = userProfile.mobileno;
+
+                    userNameTextView.setText(username);
+                    useremailTextView.setText(email);
+                    userMobileNumberTextView.setText(mobileno);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(UserProfile.this,"Something went Wrong", Toast.LENGTH_LONG).show();
+
+            }
         });
 
     }
