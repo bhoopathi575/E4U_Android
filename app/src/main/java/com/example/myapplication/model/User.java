56db972642dc49e5248afd04d69a085287748b37
bhoopathi <bhoopathi575@gmail.com>
2022-05-26 16:29:05 -0400

    updated

---
+++ app/src/main/java/com/example/myapplication/model/User.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.model;
+
+public class User {
+    public String fullname,emailid,mobileno;
+    public User(){
+
+    }
+    public User(String fullname,String emailid,String mobileno){
+        this.fullname = fullname;
+        this.emailid = emailid;
+        this.mobileno = mobileno;
+    }
+}
