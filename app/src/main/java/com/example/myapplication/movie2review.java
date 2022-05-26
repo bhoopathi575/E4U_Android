d27d924deb0a754133e45caf36ca88a02f2a9fc1
haneesha <haneeshashaik25@gmail.com>
2022-05-26 16:41:53 -0400

    updated

---
+++ app/src/main/java/com/example/myapplication/movie2review.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class movie2review extends AppCompatActivity implements View.OnClickListener{
+    public Button button;
+    public TextView textView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews_screen);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        button = (Button) findViewById(R.id.movie1bookbtn);
+        button.setOnClickListener(this);
+
+        textView = (TextView) findViewById(R.id.movie1review);
+        textView.setText("Movie Name: Acharya\n" +
+                "Directed by: Koratala Siva\n" +
+                "Starring: Chiranjeevi, Ram Charan, Pooja Hegde, Kajal Aggarwal, Sonu Sood, Jisshu Sengupta, Vennela Kishore, Saurav Lokesh, Kishore, Posani Krishna Murali, Tanikella Bharani, Ajay, Sangeetha Krish, Nassar, Banerjee, Regina Cassandra\n" +
+                "Genre: Drama, Action\n" +
+                "Release Date: 29 April, 2022\n" +
+                "Language: Telugu\n" +
+                "Running Time: – Minutes\n" +
+                "Rating: –  \n" +
+                "Budget: ₹140 Crore");
+
+        ImageView imgView=(ImageView) findViewById(R.id.movie1img);
+        imgView.setImageResource(R.drawable.movie2);
+
+        textView = (TextView) findViewById(R.id.movie1price);
+        textView.setText("30$");
+        textView = (TextView) findViewById(R.id.movie1name);
+        textView.setText("Salar");
+
+
+
+
+
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.movie1:
+                startActivity(new Intent(movie2review.this, TimingsScreen.class));
+                break;
+            case R.id.movie1bookbtn:
+                startActivity(new Intent(movie2review.this, Bookseats.class));
+                break;
+        }
+    }
+}
